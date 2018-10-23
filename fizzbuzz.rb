ab927e17e7f1bce6a43344c666784f424bc88760
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-23 18:10:41 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,14 +2,14 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0 
+  if int % 5 == 0 && int % 3 == 0
+    puts "FizzBuzz"
+    int % 3 == 0 
     puts "Fizz"
     elsif 
     int % 5 == 0 
     puts "Buzz"
     elsif 
-    int % 15 == 0 
-    puts "FizzBuzz"
   else
     puts "nil"
 end
